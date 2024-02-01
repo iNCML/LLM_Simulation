5457281b17184afdc63f2f70e1de13fd736367af
Hui Jiang <huijiang@data.eecs.yorku.ca>
2024-02-01 15:11:00 -0500

    corrected a typo in exp1.sh

--- exp1.sh
+++ exp1.sh
@@ -5,6 +5,7 @@ eps=0.0
 echo python3 generate_data.py -e $eps
 python3 generate_data.py -e $eps
 
+mkdir results
 for block in 512 1024 #16 32 64 128 256 512 1024
 do
     echo "python3 myTrain.py -b $block | cut -d' ' -f5,9 | grep '^[0-9]' > results/obj_eps${eps}_block_${block}.txt"
@@ -15,8 +16,8 @@ do
     
     for num in `seq 0 100 5001` 
     do	   
-	echo "python3 myTest.py -n 1000 -M /tmp/my_model_$num | grep difference | cut -d' ' -f5 >> results/dis_diff_eps${eps}_block${block}.txt"
-	python3 myTest.py -n 1000 -M /tmp/my_model_$num | grep difference | cut -d' ' -f5 >> results/dis_diff_eps${eps}_block${block}.txt
+	echo "python3 TestConvergence.py  -n 1000 -M /tmp/my_model_$num | grep difference | cut -d' ' -f5 >> results/dis_diff_eps${eps}_block${block}.txt"
+	python3 TestConvergence.py -n 1000 -M /tmp/my_model_$num | grep difference | cut -d' ' -f5 >> results/dis_diff_eps${eps}_block${block}.txt
 
     done  
 done
